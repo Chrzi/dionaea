2681e9127183994be5e4baedfaa8daf75ba05d8c
Christian Wahl <christian.wahl@abusix.com>
2022-05-11 19:37:53 +0200

    rewritten hpfeeds to work similar to nsq

--- modules/python/dionaea/hpfeeds.py
+++ modules/python/dionaea/hpfeeds.py
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2010 Mark Schloesser
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
+import base64
 
 from dionaea import IHandlerLoader, Timer
 from dionaea.core import ihandler, incident, g_dionaea, connection
@@ -16,30 +17,29 @@ import json
 import datetime
 from time import gmtime, strftime
 
-
 logger = logging.getLogger('hpfeeds')
 logger.setLevel(logging.DEBUG)
 
-#def DEBUGPERF(msg):
+# def DEBUGPERF(msg):
 #	print(msg)
-#logger.debug = DEBUGPERF
-#logger.critical = DEBUGPERF
+# logger.debug = DEBUGPERF
+# logger.critical = DEBUGPERF
 
 BUFSIZ = 16384
 
-OP_ERROR        = 0
-OP_INFO         = 1
-OP_AUTH         = 2
-OP_PUBLISH      = 3
-OP_SUBSCRIBE    = 4
+OP_ERROR = 0
+OP_INFO = 1
+OP_AUTH = 2
+OP_PUBLISH = 3
+OP_SUBSCRIBE = 4
 
-MAXBUF = 1024**2
+MAXBUF = 10 * (1024 ** 2)
 SIZES = {
-    OP_ERROR: 5+MAXBUF,
-    OP_INFO: 5+256+20,
-    OP_AUTH: 5+256+20,
-    OP_PUBLISH: 5+MAXBUF,
-    OP_SUBSCRIBE: 5+256*2,
+    OP_ERROR: 5 + MAXBUF,
+    OP_INFO: 5 + 256 + 20,
+    OP_AUTH: 5 + 256 + 20,
+    OP_PUBLISH: 5 + MAXBUF,
+    OP_SUBSCRIBE: 5 + 256 * 2,
 }
 CONNCHAN = 'dionaea.connections'
 CAPTURECHAN = 'dionaea.capture'
@@ -49,7 +49,7 @@ UNIQUECHAN = 'mwbinary.dionaea.sensorunique'
 
 
 class BadClient(Exception):
-        pass
+    pass
 
 
 class HPFeedsHandlerLoader(IHandlerLoader):
@@ -72,17 +72,17 @@ def timestr():
 def strpack8(x):
     if isinstance(x, str):
         x = x.encode('latin1')
-    return struct.pack('!B', len(x)%0xff) + x
+    return struct.pack('!B', len(x) % 0xff) + x
 
 
 # unpacks a string with 1 byte length field
 def strunpack8(x):
     l = x[0]
-    return x[1:1+l], x[1+l:]
+    return x[1:1 + l], x[1 + l:]
 
 
 def msghdr(op, data):
-    return struct.pack('!iB', 5+len(data), op) + data
+    return struct.pack('!iB', 5 + len(data), op) + data
 
 
 def msgpublish(ident, chan, data):
@@ -96,7 +96,7 @@ def msgsubscribe(ident, chan):
 
 
 def msgauth(rand, ident, secret):
-    auth_hash = hashlib.sha1(bytes(rand)+secret).digest()
+    auth_hash = hashlib.sha1(bytes(rand) + secret).digest()
     return msghdr(OP_AUTH, strpack8(ident) + auth_hash)
 
 
@@ -133,7 +133,7 @@ class FeedUnpack(object):
 
 
 class hpclient(connection):
-    def __init__(self, server, port, ident, secret, reconnect_timeout=10.0):
+    def __init__(self, server, port, ident, secret, reconnect_timeout=10.0, queue_max_size=100):
         logger.debug('hpclient init')
         connection.__init__(self, 'tcp')
         self.unpacker = FeedUnpack()
@@ -146,6 +146,7 @@ class hpclient(connection):
         self.filehandle = None
         self.connected = False
         self.authenticated = False
+        self.queueMax = queue_max_size
 
     def handle_established(self):
         self.connected = True
@@ -196,6 +197,9 @@ class hpclient(connection):
 
     def publish(self, channel, **kwargs):
         if self.filehandle or not self.authenticated:
+            if len(self.msgqueue) >= self.queueMax:
+                logger.warning('Discarding message because queue is full!')
+                return
             self.msgqueue.append(
                 msgpublish(self.ident, channel, json.dumps(kwargs).encode('latin1'))
             )
@@ -216,7 +220,7 @@ class hpclient(connection):
         self.filehandle = open(filepath, 'rb')
         fsize = os.stat(filepath).st_size
         headc = strpack8(self.ident) + strpack8(UNIQUECHAN)
-        headh = struct.pack('!iB', 5+len(headc)+fsize, OP_PUBLISH)
+        headh = struct.pack('!iB', 5 + len(headc) + fsize, OP_PUBLISH)
         self.send(headh + headc)
 
     def sendfiledata(self):
@@ -257,18 +261,20 @@ class hpfeedihandler(ihandler):
 
     def __init__(self, path, config=None):
         logger.debug('hpfeedhandler init')
-        reconnect_timeout = config.get("reconnect_timeout")
-        if reconnect_timeout is None:
-            reconnect_timeout = self.default_reconnect_timeout
+
+        server = os.getenv('HPFEEDS_SERVER', config['server'])
+        port = os.getenv('HPFEEDS_PORT', config.get('port', self.default_port))
+        ident = os.getenv('HPFEEDS_IDENT', config['ident'])
+        secret = os.getenv('HPFEEDS_SECRET', config['secret'])
+        self.channel = os.getenv('HPFEEDS_CHANNEL', config['channel'])
+        reconnect_timeout = os.getenv('HPFEEDS_TIMEOUT', config.get('reconnect_timeout', self.default_reconnect_timeout))
+
         try:
             reconnect_timeout = float(reconnect_timeout)
         except (TypeError, ValueError) as e:
             logger.warn("Unable to convert value '%s' for reconnect timeout to float" % reconnect_timeout)
             reconnect_timeout = self.default_reconnect_timeout
 
-        port = config.get("port")
-        if port is None:
-            port = self.default_port
         try:
             port = int(port)
         except (TypeError, ValueError) as e:
@@ -276,182 +282,271 @@ class hpfeedihandler(ihandler):
             port = self.default_port
 
         self.client = hpclient(
-            config['server'],
+            server,
             port,
-            config['ident'],
-            config['secret'],
+            ident,
+            secret,
             reconnect_timeout=reconnect_timeout
         )
         ihandler.__init__(self, path)
-
-        self.dynip_resolve = config.get('dynip_resolve', '')
-        self.dynip_timer = None
-        self.ownip = None
-        if isinstance(self.dynip_resolve, str) and self.dynip_resolve.startswith("http"):
-            logger.debug('hpfeedihandler will use dynamic IP resolving!')
-            self.dynip_timer = Timer(
-                300,
-                self._dynip_resolve,
-                delay=2,
-                repeat=True,
-            )
-            self.dynip_timer.start()
+        self.ownip = config.get('own_ip', '')
 
     def stop(self):
-        if self.dynip_timer:
-            self.dynip_timer.stop()
-            self.dynip_timer = None
+        pass
 
     def _ownip(self, icd):
-        if self.dynip_resolve and 'http' in self.dynip_resolve:
-            if self.ownip:
-                return self.ownip
-            else:
-                raise Exception('Own IP not yet resolved!')
+        if self.ownip:
+            return self.ownip
         return icd.con.local.host
 
     def __del__(self):
         # self.client.close()
         pass
 
+    def _prepare_value(self, v):
+        """
+        Prepare value to be JSON compatible.
+
+        :param v: The value to prepare.
+        :return: The prepared value
+        """
+        if isinstance(v, bytes):
+            return v.decode(encoding="utf-8", errors="replace")
+        elif isinstance(v, list) and len(v) > 0 and isinstance(v[0], bytes):
+            return [x.decode(encoding="utf-8", errors="replace") for x in v]
+        return v
+
     def connection_publish(self, icd, con_type):
         try:
-            con=icd.con
+            con = icd.con
             self.client.publish(
-                CONNCHAN,
-                connection_type=con_type,
-                connection_transport=con.transport,
-                connection_protocol=con.protocol,
-                remote_host=con.remote.host,
-                remote_port=con.remote.port,
-                remote_hostname=con.remote.hostname,
-                local_host=self._ownip(icd),
-                local_port=con.local.port
+                self.channel,
+                icd=icd.origin,
+                con_type=con_type,
+                con_transport=con.transport,
+                con_protocol=con.protocol,
+                src_addr=con.remote.host,
+                src_port=con.remote.port,
+                src_name=con.remote.hostname,
+                dst_addr=self._ownip(icd),
+                dst_dport=con.local.port
             )
         except Exception as e:
-            logger.warn('exception when publishing: {0}'.format(e))
+            logger.warning('exception when publishing', exc_info=e)
+
+    def command_serialize(self, icd, cmd, args):
+        con = icd.con
+        data = {
+            "icd": icd.origin,
+            "con_transport": con.transport,
+            "con_protocol": con.protocol,
+            "src_addr": con.remote.host,
+            "src_port": con.remote.port,
+            "src_name": con.remote.hostname,
+            "dst_addr": self._ownip(icd),
+            "dst_port": con.local.port,
+            "cmd": self._prepare_value(cmd)
+        }
+        if args:
+            data["args"] = self._prepare_value(args)
+        return data
+
+    def login_publish(self, icd):
+        try:
+            con = icd.con
+            self.client.publish(
+                self.channel,
+                icd=icd.origin,
+                con_transport=con.transport,
+                con_protocol=con.protocol,
+                src_addr=con.remote.host,
+                src_port=con.remote.port,
+                src_name=con.remote.hostname,
+                dst_addr=self._ownip(icd),
+                dst_port=con.local.port,
+                username=self._prepare_value(icd.username),
+                password=self._prepare_value(icd.password)
+            )
+        except Exception as e:
+            logger.warning('exception when publishing', exc_info=e)
 
     def handle_incident(self, i):
         pass
 
     def handle_incident_dionaea_connection_tcp_listen(self, icd):
         self.connection_publish(icd, 'listen')
-        con=icd.con
+        con = icd.con
         logger.info("listen connection on %s:%i" %
-            (con.remote.host, con.remote.port))
+                    (con.remote.host, con.remote.port))
 
     def handle_incident_dionaea_connection_tls_listen(self, icd):
         self.connection_publish(icd, 'listen')
-        con=icd.con
+        con = icd.con
         logger.info("listen connection on %s:%i" %
-            (con.remote.host, con.remote.port))
+                    (con.remote.host, con.remote.port))
 
     def handle_incident_dionaea_connection_tcp_connect(self, icd):
         self.connection_publish(icd, 'connect')
-        con=icd.con
+        con = icd.con
         logger.info("connect connection to %s/%s:%i from %s:%i" %
-            (con.remote.host, con.remote.hostname, con.remote.port, self._ownip(icd), con.local.port))
+                    (con.remote.host, con.remote.hostname, con.remote.port, self._ownip(icd), con.local.port))
 
     def handle_incident_dionaea_connection_tls_connect(self, icd):
         self.connection_publish(icd, 'connect')
-        con=icd.con
+        con = icd.con
         logger.info("connect connection to %s/%s:%i from %s:%i" %
-            (con.remote.host, con.remote.hostname, con.remote.port, self._ownip(icd), con.local.port))
+                    (con.remote.host, con.remote.hostname, con.remote.port, self._ownip(icd), con.local.port))
 
     def handle_incident_dionaea_connection_udp_connect(self, icd):
         self.connection_publish(icd, 'connect')
-        con=icd.con
+        con = icd.con
         logger.info("connect connection to %s/%s:%i from %s:%i" %
-            (con.remote.host, con.remote.hostname, con.remote.port, self._ownip(icd), con.local.port))
+                    (con.remote.host, con.remote.hostname, con.remote.port, self._ownip(icd), con.local.port))
 
     def handle_incident_dionaea_connection_tcp_accept(self, icd):
         self.connection_publish(icd, 'accept')
-        con=icd.con
+        con = icd.con
         logger.info("accepted connection from  %s:%i to %s:%i" %
-            (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
+                    (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
 
     def handle_incident_dionaea_connection_tls_accept(self, icd):
         self.connection_publish(icd, 'accept')
-        con=icd.con
+        con = icd.con
         logger.info("accepted connection from %s:%i to %s:%i" %
-            (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
+                    (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
 
     def handle_incident_dionaea_connection_tcp_reject(self, icd):
         self.connection_publish(icd, 'reject')
-        con=icd.con
+        con = icd.con
         logger.info("reject connection from %s:%i to %s:%i" %
-            (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
+                    (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
 
     def handle_incident_dionaea_connection_tcp_pending(self, icd):
         self.connection_publish(icd, 'pending')
-        con=icd.con
+        con = icd.con
         logger.info("pending connection from %s:%i to %s:%i" %
-            (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
+                    (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
+
+    def handle_incident_dionaea_connection_free(self, icd):
+        self.connection_publish(icd, 'free')
+        con = icd.con
+        logger.info("freed connection from %s:%i to %s:%i" %
+                    (con.remote.host, con.remote.port, self._ownip(icd), con.local.port))
 
     def handle_incident_dionaea_download_complete_unique(self, i):
-        self.handle_incident_dionaea_download_complete_again(i)
-        if not hasattr(i, 'con') or not self.client.connected:
+        if not hasattr(i, 'con'):
             return
         logger.debug('unique complete, publishing md5 {0}, path {1}'.format(i.md5hash, i.file))
         try:
-            self.client.sendfile(i.file)
-        except Exception as e:
-            logger.warn('exception when publishing: {0}'.format(e))
+            size = os.path.getsize(i.file)
+            fh = open(i.file, 'rb')
+            buf = fh.read(MAXBUF / 2)
 
-    def handle_incident_dionaea_download_complete_again(self, i):
-        if not hasattr(i, 'con') or not self.client.connected: return
-        logger.debug('hash complete, publishing md5 {0}, path {1}'.format(i.md5hash, i.file))
-        try:
-            tstamp = timestr()
             sha512 = sha512file(i.file)
             self.client.publish(
-                CAPTURECHAN,
-                time=tstamp,
-                saddr=i.con.remote.host,
-                sport=str(i.con.remote.port),
-                daddr=self._ownip(i),
-                dport=str(i.con.local.port),
+                self.channel,
+                icd=i.origin,
+                src_addr=i.con.remote.host,
+                src_port=i.con.remote.port,
+                dst_addr=self._ownip(i),
+                dst_port=i.con.local.port,
+                url=i.url,
+                md5=i.md5hash,
+                sha512=sha512,
+                size=size,
+                data=base64.b64encode(buf).decode("ascii")
+            )
+        except Exception as e:
+            logger.warning('exception when publishing: {0}'.format(e))
+
+    def handle_incident_dionaea_download_complete_again(self, i):
+        if not hasattr(i, 'con'):
+            return
+        logger.debug('hash complete, publishing md5 {0}, path {1}'.format(i.md5hash, i.file))
+        try:
+            sha512 = sha512file(i.file)
+            self.client.publish(
+                self.channel,
+                icd=i.origin,
+                src_addr=i.con.remote.host,
+                src_port=i.con.remote.port,
+                dst_addr=self._ownip(i),
+                dst_port=i.con.local.port,
                 md5=i.md5hash,
                 sha512=sha512,
                 url=i.url
             )
         except Exception as e:
-            logger.warn('exception when publishing: {0}'.format(e))
+            logger.warning('exception when publishing: {0}'.format(e))
 
     def handle_incident_dionaea_modules_python_smb_dcerpc_request(self, i):
-        if not hasattr(i, 'con') or not self.client.connected:
+        if not hasattr(i, 'con'):
             return
         logger.debug('dcerpc request, publishing uuid {0}, opnum {1}'.format(i.uuid, i.opnum))
         try:
             self.client.publish(
-                DCECHAN,
+                self.channel,
+                icd=i.origin,
                 uuid=i.uuid,
                 opnum=i.opnum,
-                saddr=i.con.remote.host,
-                sport=str(i.con.remote.port),
-                daddr=self._ownip(i),
-                dport=str(i.con.local.port)
+                src_addr=i.con.remote.host,
+                src_port=str(i.con.remote.port),
+                dst_addr=self._ownip(i),
+                dst_port=str(i.con.local.port)
             )
         except Exception as e:
-            logger.warn('exception when publishing: {0}'.format(e))
+            logger.warning('exception when publishing: {0}'.format(e))
 
     def handle_incident_dionaea_module_emu_profile(self, icd):
-        if not hasattr(icd, 'con') or not self.client.connected:
+        if not hasattr(icd, 'con'):
             return
         logger.debug('emu profile, publishing length {0}'.format(len(icd.profile)))
         try:
-            self.client.publish(SCPROFCHAN, profile=icd.profile)
+            self.client.publish(
+                self.channel,
+                icd=icd.origin,
+                src_addr=icd.con.remote.host,
+                src_port=icd.con.remote.port,
+                dst_addr=self._ownip(icd),
+                dst_port=icd.con.local.port,
+                profile=icd.profile
+            )
         except Exception as e:
-            logger.warn('exception when publishing: {0}'.format(e))
+            logger.warning('exception when publishing: {0}'.format(e))
 
-    def _dynip_resolve(self, events, data):
-        i = incident("dionaea.upload.request")
-        i._url = self.dynip_resolve
-        i._callback = "dionaea.modules.python.hpfeeds.dynipresult"
-        i.report()
+    def handle_incident_dionaea_modules_python_mssql_login(self, icd):
+        if not hasattr(icd, 'con'):
+            return
+        self.login_publish(icd)
 
-    def handle_incident_dionaea_modules_python_hpfeeds_dynipresult(self, icd):
-        fh = open(icd.path, mode="rb")
-        self.ownip = fh.read().strip().decode('latin1')
-        logger.debug('resolved own IP to: {0}'.format(self.ownip))
-        fh.close()
+    def handle_incident_dionaea_modules_python_mssql_cmd(self, icd):
+        if not hasattr(icd, 'con'):
+            return
+        data = self.command_serialize(icd, icd.cmd, None)
+        self.client.publish(self.channel, **data)
+
+    def handle_incident_dionaea_modules_python_mysql_login(self, icd):
+        if not hasattr(icd, 'con'):
+            return
+        self.login_publish(icd)
+
+    def handle_incident_dionaea_modules_python_mysql_command(self, icd):
+        if not hasattr(icd, 'con'):
+            return
+        if hasattr(icd, 'args'):
+            data = self.command_serialize(icd, icd.cmd, icd.args)
+        else:
+            data = self.command_serialize(icd, icd.command, None)
+        self.client.publish(self.channel, **data)
+
+    def handle_incident_dionaea_modules_python_ftp_login(self, icd):
+        if not hasattr(icd, 'con'):
+            return
+        self.login_publish(icd)
+
+    def handle_incident_dionaea_modules_python_ftp_command(self, icd):
+        if not hasattr(icd, 'con'):
+            return
+        data = self.command_serialize(icd, icd.command, icd.arguments)
+        self.client.publish(self.channel, **data)
+
